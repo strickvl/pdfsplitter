077588ab85ba7c5d57ce277a6e7faf87a2da92f5
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-12-31 15:31:23 +0100

    add initial wrapper functions

--- pdfsplitter/_nbdev.py
+++ pdfsplitter/_nbdev.py
@@ -2,8 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb",
-         "HelloSayer": "00_core.ipynb"}
+index = {"pdf_to_img": "00_core.ipynb",
+         "extract_images_from_pdfs": "00_core.ipynb"}
 
 modules = ["core.py"]
 
